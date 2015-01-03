c6b70c889b5d94eeb52bc28399dc2327e629e2e3
Boyko Anton <boyko.ant@live.com>
2015-01-03 15:16:21 +0200

    add some empty pages

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,39 @@ router.get('/', function(req, res) {
         }});
 });
 
+router.get('/agenda', function(req, res) {
+    res.render('agenda', {
+        partials: {
+            layout: "layout/base"
+        }});
+});
+
+router.get('/registration', function(req, res) {
+    res.render('registration', {
+        partials: {
+            layout: "layout/base"
+        }});
+});
+
+router.get('/speakers', function(req, res) {
+    res.render('speakers', {
+        partials: {
+            layout: "layout/base"
+        }});
+});
+
+router.get('/locations', function(req, res) {
+    res.render('locations', {
+        partials: {
+            layout: "layout/base"
+        }});
+});
+
+router.get('/live', function(req, res) {
+    res.render('live', {
+        partials: {
+            layout: "layout/base"
+        }});
 });
 
 module.exports = router;
