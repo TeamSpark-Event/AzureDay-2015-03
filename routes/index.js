5a517bf016fca819c869c78cf7bad65f9a65fc4f
Boyko Anton <boyko.ant@live.com>
2015-01-03 15:14:46 +0200

    favicon added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        partials: {
+            layout: "layout/base"
+        }});
+});
+
 });
 
 module.exports = router;
